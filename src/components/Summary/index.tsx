f7fbcbcee8e3b6488f37f5ae7bf98abf0dbcc07f
Acacio Silva <77984080+Acacio-Silva@users.noreply.github.com>
2022-02-19 15:24:13 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,7 +7,7 @@ export function Summary(){
 
     const summary = transactions.reduce((acc, transaction)=>{
 
-        if(transaction.type == 'deposit'){
+        if(transaction.type === 'deposit'){
             acc.deposits += transaction.amount;
             acc.total += transaction.amount;
         }
